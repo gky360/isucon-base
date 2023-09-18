9d62a5494ee9c62a19e9459651d2195fffc0dd5b
gky360 <gky360@gmail.com>
2023-09-18 21:46:02 +0900

    Update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 set -eux
 
+cd
+
+git diff --exit-code > /dev/null
+GIT_REVISION=$(git rev-parse --short HEAD)
+
 date -R
-echo "Started deploying."
+./dotfiles/scripts/slack.sh isucon ":arrow_forward: \`${GIT_REVISION}\` Started deploying."
+
+finally() {
+  ret=$?
+
+  date -R
+  if [ $ret -eq 0 ]; then
+    ./dotfiles/scripts/slack.sh isucon ":white_check_mark: \`${GIT_REVISION}\` Finished deploying."
+  else
+    ./dotfiles/scripts/slack.sh isucon ":large_red_square: \`${GIT_REVISION}\` Failed to deploy."
+  fi
+
+  exit $ret
+}
+trap finally EXIT
 
 # rotate logs
 function rotate_log () {
@@ -16,18 +35,16 @@ rotate_log /var/log/mysql/slow.log
 
 
 # build go app
-cd $APP_DIR
+pushd $APP_DIR
 $APP_BUILD_CMD
+popd
 
 # update mysqld.cnf
-if [ -e ~/etc/mysqld.cnf ]; then
-  sudo cp /home/isucon/etc/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+if [ -e ~/etc/mysql/mysqld.cnf ]; then
+  sudo cp ~/etc/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
 
 # restart services
 sudo systemctl restart mysql
 sudo systemctl restart $APP_SERVICE_NAME
 sudo systemctl restart nginx
-
-date -R
-echo "Finished deploying."
