45b7daebef99717bfe36d28328ea024c29f7cf22
gky360 <gky360@gmail.com>
2023-09-19 00:13:57 +0900

    Add sysctl.conf

--- manage_etc_files.sh
+++ manage_etc_files.sh
@@ -6,6 +6,7 @@ mkdir -p ~/etc/nginx
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
 sudo mv /etc/nginx/nginx.conf ~/etc/nginx/nginx.conf
 sudo chmod 666 ~/etc/nginx/nginx.conf
+sudo chown isucon:isucon ~/etc/nginx/nginx.conf
 sudo ln -s ~/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # mysql
@@ -13,3 +14,11 @@ mkdir -p ~/etc/mysql
 sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.org
 sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf ~/etc/mysql/
 sudo chmod 666 ~/etc/mysql/mysqld.cnf
+sudo chown isucon:isucon ~/etc/mysql/mysqld.cnf
+
+# sysctl
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.org
+sudo mv /etc/sysctl.conf ~/etc/sysctl.conf
+sudo chmod 666 ~/etc/sysctl.conf
+sudo chown isucon:isucon ~/etc/sysctl.conf
+sudo ln -s ~/etc/sysctl.conf /etc/sysctl.conf
